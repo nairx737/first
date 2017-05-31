094cece4c8c6bc986a77a61829092ca918896c1e
nairx737 <nairi.sargsyan@gmail.com>
2017-05-31 10:33:49 +0400

    3-rd

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("some code");
+        int change=1000;
+    }
+}
---
+++ src/com/company/norutyun.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by user on 5/31/2017.
+ */
+public class norutyun {
+    public static void main(String[] args) {
+        System.out.println("nonono");
+    }
+}
